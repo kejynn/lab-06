32b8585b545fc6370b8e04799b004f9feade7ad3
Kejynn <dimaanis622721@mail.ru>
2023-05-09 19:32:47 +0300

    fdf

---
+++ app/src/main/java/com/example/lab06/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.lab06;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayAdapter <Note> adp;
+    int sel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        adp = new ArrayAdapter<Note>(this, android.R.layout.simple_list_item_1);
+
+        ListView lst = findViewById(R.id.List);
+        lst.setAdapter(adp);
+
+        lst.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                sel = position;
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
+    {
+        if (data != null)
+        {
+            int pos = data.getIntExtra("my-note-index", -1);
+            String title = data.getStringExtra("my-note-title");
+            String content = data.getStringExtra("my-note-content");
+
+            Note n = adp.getItem(pos);
+            n.title = title;
+            n.content = content;
+
+            adp.notifyDataSetChanged();
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public void on_click_new (View v)
+    {
+        Note n = new Note();
+        n.title = "New note";
+        n.content = "Some content";
+
+        adp.add(n);
+        int pos = adp.getPosition(n);
+
+        Intent i = new Intent(this, MainActivity2.class);
+        i.putExtra("my-note-index", pos);
+        i.putExtra("my-note-title", n.title);
+        i.putExtra("my-note-content", n.content);
+
+        startActivityForResult(i, 54321);
+    }
+
+    public void on_click_edit (View v)
+    {
+        int pos = sel;
+        Note n = new Note();
+
+        Intent i = new Intent(this, MainActivity2.class);
+        i.putExtra("my-note-index", pos);
+        i.putExtra("my-note-title", n.title);
+        i.putExtra("my-note-content", n.content);
+
+        startActivityForResult(i, 54321);
+    }
+
+    public void on_click_delete (View v)
+    {
+        int pos = sel;
+        Note n = new Note();
+        adp.remove(n);
+    }
+}
---
+++ app/src/main/java/com/example/lab06/Note.java
@@ -0,0 +1,12 @@
+package com.example.lab06;
+
+public class Note
+{
+    public String title;
+    public String content;
+
+    public String toString()
+    {
+        return title;
+    }
+}
